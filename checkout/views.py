dc7828c1654692377f5ca9db88a27cc56160d376
mattingliscoding <mattingliscoding@gmail.com>
2021-02-23 00:57:16 +0000

    stripe elements added

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51INoygJuMj1zZXH4dgYXIde1CdI7lA64VpzyWprqZhEymmNQ1Y5QZtpb4ZgId2R8MRh7pAj08jXCldqBripSjiZW00A3LgQH4L',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
