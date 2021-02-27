e8261ce3389baee1c0cdabf45ac0e49e87f7a875
mattingliscoding <mattingliscoding@gmail.com>
2021-02-27 12:41:27 +0000

    data dump, objects installed

--- products/views.py
+++ products/views.py
@@ -14,6 +14,7 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    print(products)
     query = None
     categories = None
     sort = None
